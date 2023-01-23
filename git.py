73a26789ee98abe9616122e5abde4755dfc07d2b
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-01-23 15:26:26 +0530

    Update git.py

--- git.py
+++ git.py
@@ -1,31 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from threading import Thread, Barrier
 
-#below code was copied from internet source for script run 
-
-def load_proxies(PATH): # for loading the proxies
-    return open(PATH).read().split('\n') 
-
-def load_session(url,proxy): # manage each session
-    proxy,port = proxy.split(':')
-    profile = webdriver.Chrome()
-    profile.set_preference("network.proxy.type", 1)
-    profile.set_preference("network.proxy.http", proxy)
-    profile.set_preference("network.proxy.http_port", port)
-    profile.set_preference("network.proxy.ssl", proxy)
-    profile.set_preference("network.proxy.ssl_port", port)
-    profile.update_preferences()
-    
 #Below Code Is Copied From Stackoverflow
 def func(barrier):
     
     driver = webdriver.Chrome()
-
-    driver.get("https://github.com/xtekky")  #change to your github url
+    driver.set_window_size(300,400)
+    time.sleep(5)
+    size = driver.get_window_size()
+    driver.get("https://github.com/google")  #change to your github url
     time.sleep(20)
     driver.close()
     
@@ -35,7 +21,7 @@ def func(barrier):
     print('click')
     b.click()
 
-number_of_threads = 10 #how much views you want change to your confort
+number_of_threads = 2 #how much views you want change to your needed number
 
 barrier = Barrier(number_of_threads)
 
